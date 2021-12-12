1599f4213fbaeb065196a25f6e7b5163c87f70dc
ssentinull <ibnu.muhari@gmail.com>
2021-12-12 13:28:40 +0700

    test: calculateVectors func call in create func tests

--- pkg/article/usecase/article_usecase_test.go
+++ pkg/article/usecase/article_usecase_test.go
@@ -17,6 +17,7 @@ func TestCreateArticle(t *testing.T) {
 
 	t.Run("success", func(t *testing.T) {
 		mockRepo.On("Create", mock.Anything, mock.AnythingOfType("*model.Article")).Return(nil).Once()
+		mockRepo.On("CalculateVectors", mock.Anything, mock.AnythingOfType("*model.Article")).Return(nil).Once()
 		mockUsecase := _articleUcase.NewArticleUsecase(mockRepo)
 		err := mockUsecase.Create(context.TODO(), mockArticle)
 
@@ -24,13 +25,25 @@ func TestCreateArticle(t *testing.T) {
 		mockRepo.AssertExpectations(t)
 	})
 
-	t.Run("failed", func(t *testing.T) {
+	t.Run("failed-create", func(t *testing.T) {
 		mockRepo.On("Create", mock.Anything, mock.AnythingOfType("*model.Article")).Return(model.ErrBadRequest).Once()
 		mockUsecase := _articleUcase.NewArticleUsecase(mockRepo)
 		err := mockUsecase.Create(context.TODO(), mockArticle)
 
 		assert.Error(t, err)
 		assert.Equal(t, model.ErrBadRequest, err)
+		mockRepo.AssertExpectations(t)
+	})
+
+	t.Run("failed-calculate-vectors", func(t *testing.T) {
+		mockRepo.On("Create", mock.Anything, mock.AnythingOfType("*model.Article")).Return(nil).Once()
+		mockRepo.On("CalculateVectors", mock.Anything, mock.AnythingOfType("*model.Article")).
+			Return(model.ErrInternalServer).Once()
+		mockUsecase := _articleUcase.NewArticleUsecase(mockRepo)
+		err := mockUsecase.Create(context.TODO(), mockArticle)
+
+		assert.Error(t, err)
+		assert.Equal(t, model.ErrInternalServer, err)
 		mockRepo.AssertExpectations(t)
 	})
 }
